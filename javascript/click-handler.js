e9102a635c4cabdbbd051e6729f3161a1b8d28da
bassplayer7 <jpmowing@gmail.com>
2014-01-02 16:49:53 -0600

    It now tells me what key I pressed

--- javascript/click-handler.js
+++ javascript/click-handler.js
@@ -1,3 +1,25 @@
 /**
  * Created by jessemaxwell on 1/2/14.
  */
+function mouseeventHandler(event) {
+    if (!event) event = window.event;
+
+    var eType = event.type;
+    var eTarget = event.target || event.srcElement;
+    alert(eType + ' event on element with id: '+ eTarget.id)
+}
+
+function onloadHandler () {
+    var body = document.body;
+    var span = document.createElement('span');
+    span.id = 'ExampleSpan';
+    span.appendChild(document.createTextNode ('Click Here!'));
+
+    span.onmousedown = mouseeventHandler;
+    span.onmouseup = mouseeventHandler;
+    span.onmouseover = mouseeventHandler;
+
+    body.appendChild(span);
+}
+
+window.onload = onloadHandler;
