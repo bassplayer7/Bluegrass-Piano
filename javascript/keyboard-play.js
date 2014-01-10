fd8627c5c589b0371e365880ccc8b9d62ccb393c
bassplayer7 <jpmowing@gmail.com>
2014-01-10 12:04:53 -0600

    First iteration of JS piano. Basically works like the :hover did

--- javascript/keyboard-play.js
+++ javascript/keyboard-play.js
@@ -2,65 +2,72 @@
  * The goal of this is to make all the keyboard notes play.
  */
 
-var whiteScale = [0, 2, 4, 5, 7, 9, 11, 12];
-var blackScale = [1, 3, 6, 8, 10];
 
-var keyBoard = document.getElementById('keyBoard');
-var whiteKey = keyBoard.getElementsByTagName('a');
 
-var keyboardNavigation =  {
+//var keyBoard = document.getElementById('keyBoard');
+//var whiteKey = keyBoard.getElementsByTagName('a');
 
-    addActive: function(key) {
-        if (key.className !== "active") {
-            key.className += " active";
-        }
-    },
-    removeActive: function(key) {
-        if (key.className === " active") {
-            key.className -= " active";
-        }
-    },
-    addEvent: function(obj, type, fn) {
-        if ( obj.attachEvent) {
-            obj['e'+type+fn] = fn;
-            obj[type+fn] = function(){obj['e'+type+fn] (window.event); };
-            obj.attachEvent('on'+type, obj[type_fn] );
-        } else {
-            obj.addEventListener( type, fn, false );
-        }
-    },
+var Navigation = function() {
+    var base = this;
 
-    initialize: function() {
-        for (var i=0; i<whiteKey.length - 1; i++) {
-            var note = whiteKey[i];
-            keyboardNavigation.addEvent(note, "mouseover", function(e) {
-                var target = window.event ? window.event.srcElement : e.target;
-                keyboardNavigation.addActive(target);
-            }, true);
-            keyboardNavigation.addEvent(note, "mouseout", function(e) {
-                var target = window.event ? window.event.srcElement : e.target;
-                keyboardNavigation.removeActive(target);
-            }, true)
+    base.keyboard = document.getElementById('keyBoard');
+    base.whiteKey = base.keyboard.getElementsByTagName('a');
+
+    base.addActive = function(e) {
+        var pressedKey = e.target;
+        pressedKey.className += " active";
+        pressedKey.addEventListener('mouseout', base.removeActive);
+    };
+
+    base.removeActive = function(e) {
+        var target = e.target,
+            parent = e.target.offsetParent;
+
+        base.replaceClassName(target);
+
+        if (parent.tagName === "A") {
+            base.replaceClassName(parent);
         }
+    };
+
+
+    base.replaceClassName = function(input) {
+        input.className = input.className.replace(/(?:^|\s)active(?!\S)/g , '' );
+        return base;
+    };
+
+    base.keepActive = function(e) {
+        var parent = e.target.offsetParent;
+        if (parent.tagName === "A") {
+            parent.className += " active";
+            e.target.addEventListener('mouseout', base.removeActive)
+        }
+    };
+
+    base.findKeys = function() {
+        var i = 0;
+        for (; i < base.whiteKey.length; i++) {
+            var whiteKey = base.whiteKey[i];
+
+            whiteKey.addEventListener('mouseover', base.addActive, true);
+            if (whiteKey.getElementsByTagName('span').length > 0) {
+                whiteKey.getElementsByTagName('span')[0].addEventListener('mouseover', base.keepActive, true);
+            }
+        }
+    };
+
+    base.getWhiteScale = function() {
+        return [0, 2, 4, 5, 7, 9, 11, 12];
+    };
+
+    base.getBlackScale = function() {
+        return [1, 3, 6, 8, 10];
     }
-};
-keyboardNavigation.addEvent(window, "load", keyboardNavigation.initialize);
+}
+
+var navigation = new Navigation();
+window.addEventListener("load", navigation.findKeys, true);
 
 
-//whiteKey.onmousedown = keyboardNavigation();
 
-//var javascriptWork = function() {
-//    var theHtmlNode = document.childNodes[1];
-//    var theParagraphNode = document.getElementById('keyBoard');
-//    var allParagraphs = document.getElementsByTagName('p')
-//    if (!theHtmlNode) {
-//        alert( "theHtmlNode is a " + theHtmlNode.nodeName + " node!" );
-//    }
-//
-//    getwidth = function() {
-//        return allParagraphs[1].clientWidth;
-//        alert("theParagraphNode is a " + theParagraphNode + " node." );
-//    }
-//    return true;
-//
-//};
+
