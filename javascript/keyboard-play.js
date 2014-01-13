b31da0012acbd4fdeb217d062f9dc650f8ba6e69
bassplayer7 <jpmowing@gmail.com>
2014-01-13 10:36:37 -0600

    Improved the keyboard playing darastically.

--- javascript/keyboard-play.js
+++ javascript/keyboard-play.js
@@ -6,11 +6,11 @@ var Navigation = function() {
     var base = this;
 
     base.keyboard = document.getElementById('keyboard');
-    base.whiteKey = base.keyboard.getElementsByTagName('li');
-    base.totalKeys = base.whiteKey.length;
+//    base.whiteKey = base.keyboard.getElementsByTagName('li');
+    base.pianoKey = base.keyboard.getElementsByTagName('li');
+    base.totalKeys = base.pianoKey.length;
     base.docHeight = document.childNodes[1].scrollHeight - document.childNodes[1].clientHeight;
     base.rangeOfKey = base.docHeight / base.totalKeys - 1;
-    base.activeElements = 0;
 
     // This adds the "active" class to the keys when they are hovered.
     base.addActive = function(e) {
@@ -33,8 +33,10 @@ var Navigation = function() {
 
     // Finds and replaces the "active" class.
     base.replaceClassName = function(input) {
-        input.className = input.className.replace(/(?:^|\s)active(?!\S)/g , '' );
-        return base;
+        if (input !== undefined) {
+            input.className = input.className.replace(/(?:^|\s)active(?!\S)/g , '' );
+            return base;
+        }
     };
 
     // If the parent is a <li /> then keep the active on.
@@ -46,10 +48,12 @@ var Navigation = function() {
         }
     };
 
+    // Finds all the piano keys and allows them to be hovered. It also allows for the addition of an <a />
+    // and prevents it from flashing "active".
     base.findKeys = function() {
         var i = 0;
-        for (; i < base.whiteKey.length; i++) {
-            var whiteKey = base.whiteKey[i];
+        for (; i < base.pianoKey.length; i++) {
+            var whiteKey = base.pianoKey[i];
 
             whiteKey.addEventListener('mouseover', base.addActive, true);
             if (whiteKey.getElementsByTagName('a').length > 0) {
@@ -58,41 +62,33 @@ var Navigation = function() {
         }
     };
 
-    base.currentKey = function() {
-        var arr = [];
-        var i = 0;
-        for (; i < base.whiteKey.length; i++) {
-            arr.push(base.rangeOfKey * i);
-        }
-        return arr;
-    };
-
-    base.removeActiveScroll = function(pos) {
-        base.replaceClassName(base.whiteKey[pos]);
-    };
-
+    // This rounds the scrollY number so that it can be used to access values in the pianoKey array.
+    // It will then add the "active" class to that specific key.
+    // It saves the current key to the lastKey variable so it knows what was the last "active" tag.
     base.pressKeys = function() {
-        var currentScroll = window.scrollY;
-
-        var i = 0;
-        for (; i < base.position.length; i++) {
-            if (currentScroll >= base.position[i] && currentScroll <= (base.position[i] + base.rangeOfKey)) {
-                base.whiteKey[i].className += " active";
-                base.activeElements = i;
-            } else {
-                base.removeActiveScroll(i);
-            }
+        if (base.lastKey !== undefined) {
+            base.replaceClassName(base.pianoKey[base.lastKey]);
         }
+
+        var keyCode = Math.round((window.scrollY/base.docHeight) * base.totalKeys),
+            key = base.pianoKey[keyCode];
+
+        if (key !== undefined) {
+            key.className += " active";
+        }
+
+        base.lastKey = keyCode;
     };
 
+    // This loads the functions.
     base.load = function() {
-        base.findKeys();
-        base.currentKey();
-        base.pressKeys()
-        base.position = base.currentKey();
+        base.lastKey = 0;
 
-        window.addEventListener("scroll", navigation.pressKeys, true);
-    }
+        base.findKeys();
+        base.pressKeys();
+
+        window.addEventListener("scroll", base.pressKeys, true);
+    };
 };
 
 var navigation = new Navigation();
