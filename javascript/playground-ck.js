caf8a79a308f4a431fb7548e9725c7901ac203ff
Jesse Maxwell <jessemaxwell@Jesses-MacBook-Pro.local>
2014-03-19 18:48:43 -0500

    Made more changes

---
+++ javascript/playground-ck.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jessemaxwell on 3/6/14.
+ *///    Filter = function() {
+//        $(this).spam = function() {
+//            console.log("This seems to work.")
+//        }
+//    }
+var filter={forms:4,values:2,FilterSpam:function(){console.log("This seems to work.");var e={divs:$("div").eq(),navs:$("nav")[0]}}},disc=filter.prototype;console.log(disc);console.log(filter.FilterSpam().divs);
